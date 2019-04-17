d67633991f1c3564eff341670ae50f3c7cc53300
unknown <islam.sayed@ISLAM-SAYED-REP.EL-EZABYGROUP.LOCAL>
2019-04-17 14:30:58 +0200

    add keydown listener for input file label

--- src/App.js
+++ src/App.js
@@ -40,14 +40,12 @@ class App extends Component {
   };
   calcMrpSum = workbook => {
     var mrpSum = 0;
-    console.log(workbook);
     for (var i = 2; i < contribution.length; i++) {
       mrpSum += workbook.Sheets[workbook.SheetNames[0]][`M${i}`].v;
     }
     this.setState({
       mrpSum
     });
-    console.log(mrpSum);
   };
 
   calcAverage = number => {
@@ -66,6 +64,14 @@ class App extends Component {
   handleFileSelect = e => {
     const label = document.querySelector('.typeBorder')
     label.querySelector('span').innerText = e.target.value.split( '\\' ).pop();
+  }
+
+  handleKeyDown = e => {
+    if (e.keyCode === 13 || e.keyCode === 32) {
+      const file = document.querySelector('.type')
+      file.click()
+    }
+    
   }
 
   handleClick = e => {
@@ -102,7 +108,6 @@ class App extends Component {
                       (this.state.average / this.state.mrpSum)
                   )
                 };
-                console.log(workbook.Sheets[workbook.SheetNames[0]][`M${n}`]);
               }
             });
           }
@@ -128,12 +133,11 @@ class App extends Component {
                       (this.state.average / this.state.mrpSum)
                   )
                 };
-                console.log(workbook.Sheets[workbook.SheetNames[0]][`M${n}`]);
               }
             });
           }
         }
-        XLSX.writeFile(workbook, "testo.xlsx");
+        XLSX.writeFile(workbook, `calc-${type}-${number}.xlsx`);
       };
 
       reader.readAsArrayBuffer(file);
@@ -187,7 +191,7 @@ class App extends Component {
           <br />
           <br />
           <br />
-          <label htmlFor="file" className="typeBorder ">
+          <label htmlFor="file" onKeyDown={this.handleKeyDown} className="typeBorder" tabIndex="0">
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17"><path fill="#000000de" d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"></path></svg>
           <input type="file" id="file" accept="*.xlsx" onChange={this.handleFileSelect} className="type" required />
           <span>Choose a file</span>
